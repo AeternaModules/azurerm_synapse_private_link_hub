94c56cf1adcaa714818dba23b2f6cfa98466fb0f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:07 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "synapse_private_link_hubs" {
-  description = "All synapse_private_link_hub resources"
-  value       = azurerm_synapse_private_link_hub.synapse_private_link_hubs
-}
 output "synapse_private_link_hubs_location" {
-  description = "List of location values across all synapse_private_link_hubs"
-  value       = [for k, v in azurerm_synapse_private_link_hub.synapse_private_link_hubs : v.location]
+  description = "Map of location values across all synapse_private_link_hubs, keyed the same as var.synapse_private_link_hubs"
+  value       = { for k, v in azurerm_synapse_private_link_hub.synapse_private_link_hubs : k => v.location }
 }
 output "synapse_private_link_hubs_name" {
-  description = "List of name values across all synapse_private_link_hubs"
-  value       = [for k, v in azurerm_synapse_private_link_hub.synapse_private_link_hubs : v.name]
+  description = "Map of name values across all synapse_private_link_hubs, keyed the same as var.synapse_private_link_hubs"
+  value       = { for k, v in azurerm_synapse_private_link_hub.synapse_private_link_hubs : k => v.name }
 }
 output "synapse_private_link_hubs_resource_group_name" {
-  description = "List of resource_group_name values across all synapse_private_link_hubs"
-  value       = [for k, v in azurerm_synapse_private_link_hub.synapse_private_link_hubs : v.resource_group_name]
+  description = "Map of resource_group_name values across all synapse_private_link_hubs, keyed the same as var.synapse_private_link_hubs"
+  value       = { for k, v in azurerm_synapse_private_link_hub.synapse_private_link_hubs : k => v.resource_group_name }
 }
 output "synapse_private_link_hubs_tags" {
-  description = "List of tags values across all synapse_private_link_hubs"
-  value       = [for k, v in azurerm_synapse_private_link_hub.synapse_private_link_hubs : v.tags]
+  description = "Map of tags values across all synapse_private_link_hubs, keyed the same as var.synapse_private_link_hubs"
+  value       = { for k, v in azurerm_synapse_private_link_hub.synapse_private_link_hubs : k => v.tags }
 }
 
